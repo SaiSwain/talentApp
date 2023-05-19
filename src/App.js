f9153ebfcdeb23acc813c8cd8a4473044af2222a
Code With Harry <swainsaiprasad964@gmail.com>
2023-05-19 18:46:34 +0530

    complete navbar login page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@
 import React from "react"
 import Navbar from "./Component/Navbar";
 import Home from "./Component/Home";
-import Com from "./Component/Com";
+import Slide from "./Component/Slide";
+import Button from "./Component/Button";
 import {BrowserRouter,Routes,Route}from "react-router-dom";
 const App=()=>{
   return(
     <div>
       <BrowserRouter>
-      
       <Routes>
         {/* <Route path="/job/search"element={<Home/>}></Route> */}
         <Route index path="/"element={<Navbar/>}></Route>
         <Route path="/job/search" element={<Home/>}/>
+        <Route path="/job" element={<Slide/>}/>
+        <Route path="/register" element={<Button/>}/>
       </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Component/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Button.css"
+const Button=()=>{
+    return(
+        <div>
+           <div class="co">
+		<div className='tr'>
+    <span class="top-word"style={{fontSize:"bold"}}>TM</span>
+    <span class="bottom-word">true<span style={{fontSize:"25px"}}><span style={{fontSize: "25px"
+	}}className='la'>T</span>alent</span></span><p style={{marginLeft:"-590px",fontSize:'15px',marginTop:"-30px"}}>TT Essential</p><p  style={{marginLeft:"-350px",fontSize:'14px',marginTop:"-34px"}}>TT Gig</p>
+	<table style={{marginLeft:"700px",marginBottom:"-0px",marginTop:"-31px"}}>
+		<tr><td><button className='login'>Login</button></td>
+		<td><button  className='Register'>Register</button></td></tr></table>
+	  </div></div>
+      <h1>sai</h1>
+        </div>
+    )
+}
+export default Button;
