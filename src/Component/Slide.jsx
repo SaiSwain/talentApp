018313bef6a11dd5a88013fd3d7551516601e96d
Code With Harry <swainsaiprasad964@gmail.com>
2023-05-24 18:34:31 +0530

    complete rendering page

--- src/Component/Slide.jsx
+++ src/Component/Slide.jsx
@@ -1,12 +1,26 @@
 
 import "./Slide.css"
-import React, { useState } from 'react';
+import React, { useState,useHistory } from 'react';
 import login from"./images/login.png"
 import {Link,NavLink,useNavigate}from "react-router-dom"
-
+<script src="https://accounts.google.com/gsi/client" async defer></script>
 const Slide=()=>{
   const [showPassword, setShowPassword] = useState(false);
   const navigate=useNavigate();
+ 
+    const handleGoogleSignIn = () => {
+      const googleAuth = window.google.accounts.id.initialize({
+        client_id: 'YOUR_CLIENT_ID',
+        callback: handleSignInCallback
+      });
+  
+      googleAuth.signIn();
+    };
+  
+    const handleSignInCallback = (response) => {
+      console.log(response);
+      // Handle the sign-in response here
+    };
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
@@ -20,7 +34,7 @@ const Slide=()=>{
     
    
   return(
-    <div>
+    <div id="sat">
       <div class="co">
 		<div className='tr'>
     <span class="top-word"style={{fontSize:"bold"}}>TM</span>
@@ -30,15 +44,19 @@ const Slide=()=>{
 		<tr><td><button onClick={improve1} className='login'>Login</button></td>
 		<td><button onClick={improve2} className='Register'>Register</button></td></tr></table>
 	  </div></div>
-   <div className="imagess"><img src={login}className="i"></img>
-    <h1 style={{marginTop:"400px",marginRight:"-900px",marginLeft:"-460px",fontSize:"25px"}}>Experience Hiring 2.0 with<span style={{color:"green"}}>TrueTalent</span> </h1>
+    <div id="umb">
+   <div className="imagess"><img src={login}className="i"></img>   </div>
+    <h1 style={{marginTop:"400px",marginRight:"-1210px",marginLeft:"-150px",fontSize:"25px"}}>Experience Hiring 2.0 with<span style={{color:"#14BC9A"}}>TrueTalent</span> </h1>
     <input type="btn"className="batt" placeholder="Learn more about Truetalent"></input><h1 >sai</h1>
-    </div>
+    <div className="c10">
+          <input className="e9" placeholder="Log in to TrueTalent"></input>
+        </div>
  
-    <div className="umb">
+    </div>
+   
       <form className="f">
         <div className="c0">
-          <h1 className="e3"style={{marginBottom:"100px",marginTop:"-600px"}}>Log in to TrueTalent</h1>
+          <h1 className="e3"style={{marginBottom:"100px",marginTop:"-530px",marginRight:"-320px",marginLeft:"-10px",fontSize:"20px"}}>Log in to TrueTalent</h1>
         </div>
       <div className="c1">
       <input type="text"placeholder="Email"className="pu"></input>
@@ -57,18 +75,18 @@ const Slide=()=>{
       <button type="text"placeholder="Sign In"className="ban">Sign In</button>
       </div> 
       <div className="c4">
-      <hr className="g"></hr>OR<hr className="k"></hr>
+      <h6 className="hg1">-----------------------------OR-----------------------------</h6>
       </div>
       <div className="c5">
-      <button type="text"placeholder="Email"className="cd">Continue With Google</button>
+      <button type="text"placeholder="Email"className="cd"onClick={handleGoogleSignIn}>
+      
+        Continue With Google</button>
       </div> 
       <div className="c6">
         <h5 className="jo">(for Job Seekers only)</h5>
       </div>
       </form>
-      <div className="c10">
-          <input className="e9" placeholder="Log in to TrueTalent"></input>
-        </div></div>
+      
     </div>
   )
 }
