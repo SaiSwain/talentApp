4ad48482ce91b7d276a53e5cfe418e35a9b81e07
Code With Harry <swainsaiprasad964@gmail.com>
2023-05-30 18:31:01 +0530

    responsive

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useMediaQuery } from 'react-responsive';
 import "./Button.css"
 import login from"./images/login.png"
 import { BsPerson } from "react-icons/bs";
@@ -8,6 +9,9 @@ import emp from "./images/emp.svg";
 import Doct from "./Doct";
 import {Link,NavLink,useNavigate}from "react-router-dom"
 const Button=()=>{
+  const isDesktop = useMediaQuery({ minWidth: 992 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   let navigate = useNavigate();
   let navbar1=()=>{
     navigate("/reg1")
@@ -16,37 +20,139 @@ const Button=()=>{
       navigate("/reg2")
       }
     return(
-        <div>
+      <div>
+      <header>
+        <nav>
+          {isDesktop && (
+            <div class="co">
+             	<div className='tr'>
+             <span className="top-word"style={{fontSize:"bold"}}>TM</span>
+              <span className="bottom-word">true<span style={{fontSize:"25px"}}><span style={{fontSize: "25px"
+            }}className='la'>T</span>alent</span></span><p style={{marginLeft:"-590px",fontSize:'15px',marginTop:"-30px"}}>TT Essential</p><p  style={{marginLeft:"-350px",fontSize:'14px',marginTop:"-34px"}}>TT Gig</p>
+             <table style={{marginLeft:"700px",marginBottom:"-0px",marginTop:"-31px"}}>
+             	<tr><td><button className='login'>Login</button></td>
+             	<td><button  className='Register'>Register</button></td></tr></table>
+               </div></div>
+          )}
+          {isTablet && (
+            <div class="co">
+            	<div className='tr'>
+              <span className="top-word"style={{fontSize:"bold"}}>TM</span>
+              <span className="bottom-word">true<span style={{fontSize:"25px"}}><span style={{fontSize: "25px"
+            }}className='la'>T</span>alent</span></span><p style={{marginLeft:"-590px",fontSize:'15px',marginTop:"-30px"}}>TT Essential</p><p  style={{marginLeft:"-350px",fontSize:'14px',marginTop:"-34px"}}>TT Gig</p>
+           <table style={{marginLeft:"700px",marginBottom:"-0px",marginTop:"-31px"}}>
+            <tr><td><button className='login'>Login</button></td>
+            <td><button  className='Register'>Register</button></td></tr></table>
+            </div></div>
+          )}
+        </nav>
+      </header>
+
+      <main>
+        <section className="imagess" >
          
-      <div class="co">
-		<div className='tr'>
-    <span class="top-word"style={{fontSize:"bold"}}>TM</span>
-    <span class="bottom-word">true<span style={{fontSize:"25px"}}><span style={{fontSize: "25px"
-	}}className='la'>T</span>alent</span></span><p style={{marginLeft:"-590px",fontSize:'15px',marginTop:"-30px"}}>TT Essential</p><p  style={{marginLeft:"-350px",fontSize:'14px',marginTop:"-34px"}}>TT Gig</p>
-	<table style={{marginLeft:"700px",marginBottom:"-0px",marginTop:"-31px"}}>
-		<tr><td><button className='login'>Login</button></td>
-		<td><button  className='Register'>Register</button></td></tr></table>
-	  </div></div>
-    <div className="imagess"><img src={login}className="i"></img>
-    <h1 style={{marginTop:"400px",marginRight:"-900px",marginLeft:"-460px",fontSize:"25px"}}>Experience Hiring 2.0 with<span style={{color:"green"}}>TrueTalent</span> </h1>
-    <input type="btn"className="batt" placeholder="Learn more about Truetalent"></input><h1 >sai</h1>
+            {isMobile &&  <div><img src={login}className="i"></img>
+   <h1 style={{marginTop:"400px",marginRight:"-0px",marginLeft:"-460px",fontSize:"25px"}}>Experience Hiring 2.0 with<span style={{color:"green"}}>TrueTalent</span> </h1>
+   </div>
+    }
+            {isTablet && <div><img src={login}className="i"></img>
+   <h1 style={{marginTop:"400px",marginRight:"-0px",marginLeft:"-460px",fontSize:"25px"}}>Experience Hiring 2.0 with<span style={{color:"green"}}>TrueTalent</span> </h1>
+   </div>}
+            {isDesktop && <div><img src={login}className="i"></img>
+   <h1 style={{marginTop:"400px",marginRight:"-0px",marginLeft:"-460px",fontSize:"25px"}}>Experience Hiring 2.0 with<span style={{color:"green"}}>TrueTalent</span> </h1>
+   </div>}
+            
+         
+        </section>
+
+        <section className="f1">
+          <div id="mf">
+            {isDesktop && (
+              <>
+             <div id="fbo"onClick={navbar1}>
+      <img src={can}className="jo2"/>
+       <p id="em2">I'm an employer</p>
+        <p id="em1">I am here to find my next Job</p>
+     </div>
+     <div id="sbo"onClick={navbar2}>
+   <img src={emp} className="bv"/>
+     <p id="em2">I'm an employer</p>
+     <p id="em1">I am here to look for job<br/> seeker</p>
+   
+     </div>
+              </>
+            )}
+            {(isTablet || isMobile) && (
+              <>
+                <div id="fbo"onClick={navbar1}>
+      <img src={can}className="jo2"/>
+       <p id="em2">I'm an employer</p>
+        <p id="em1">I am here to find my next Job</p>
+     </div>
+     <div id="sbo"onClick={navbar2}>
+   <img src={emp} className="bv"/>
+     <p id="em2">I'm an employer</p>
+     <p id="em1">I am here to look for job<br/> seeker</p>
+   
+     </div>
+              </>
+            )}
+          </div>
+
+          <div id="mf">
+            {isDesktop && (
+              <>
+                
+              </>
+            )}
+            {(isTablet || isMobile) && (
+              <>
+             
+              </>
+            )}
+          </div>
+        </section>
+
+        <section className="c101">
+          <footer>
+          <button className="e91" placeholder="Log in to TrueTalent">Log in to TrueTalent</button>
+          </footer>
+        </section>
+      </main>
     </div>
-      <form className="f1">
-     <div className="c11"onClick={navbar1}>
-      <p type="text"placeholder="Email"className="pu1"style={{marginLeft:"10px"}}>I'm an employer</p>
-      <img src={emp} className="bv"/>
-      <p style={{marginLeft:"-110px",marginTop:"200px",marginRight:"-190px",fontSize:"15px"}}id="hg">I am here to look for job<br/> seeker</p>
-      </div> 
-      <div className="c61"onClick={navbar2}>
-        <p className="jo1">I'm a candidate</p>
-        <img src={can}className="jo2"/>
-        <p style={{marginRight:"-500px",marginLeft:"-20px",marginTop:"200px",fontSize:"15px"}}>I am here to find my next Job</p>
-      </div>
-      </form>
-      <div className="c101">
-          <input className="e91" placeholder="Log in to TrueTalent"></input>
-        </div>
-        </div>
+  //       <div id="fer">
+  //        <div className="co1">
+  //     <div class="co">
+	// 	<div className='tr'>
+  //   <span className="top-word"style={{fontSize:"bold"}}>TM</span>
+  //   <span className="bottom-word">true<span style={{fontSize:"25px"}}><span style={{fontSize: "25px"
+	// }}className='la'>T</span>alent</span></span><p style={{marginLeft:"-590px",fontSize:'15px',marginTop:"-30px"}}>TT Essential</p><p  style={{marginLeft:"-350px",fontSize:'14px',marginTop:"-34px"}}>TT Gig</p>
+	// <table style={{marginLeft:"700px",marginBottom:"-0px",marginTop:"-31px"}}>
+	// 	<tr><td><button className='login'>Login</button></td>
+	// 	<td><button  className='Register'>Register</button></td></tr></table>
+	//   </div></div>
+  //   <div className="imagess"><img src={login}className="i"></img>
+  //   <h1 style={{marginTop:"400px",marginRight:"-0px",marginLeft:"-460px",fontSize:"25px"}}>Experience Hiring 2.0 with<span style={{color:"green"}}>TrueTalent</span> </h1>
+  
+  //   </div>
+  //     <form className="f1">
+  //       <div id="mf">
+  //       <div id="fbo"onClick={navbar1}>
+  //       <img src={can}className="jo2"/>
+  //       <p id="em2">I'm an employer</p>
+  //       <p id="em1">I am here to find my next Job</p>
+  //    </div>
+  //    <div id="sbo"onClick={navbar2}>
+  //    <img src={emp} className="bv"/>
+  //    <p id="em2">I'm an employer</p>
+  //    <p id="em1">I am here to look for job<br/> seeker</p>
+   
+  //     </div></div>
+  //     </form>
+  //     <div className="c101">
+  //         <button className="e91" placeholder="Log in to TrueTalent">Log in to TrueTalent</button>
+  //       </div>
+  //       </div></div>
     )
 }
 export default Button;
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import "./Navbar.css"
+import { useMediaQuery } from 'react-responsive';
 import { FaSistrix} from "react-icons/fa";
 import { HiMapPin } from "react-icons/hi2";
 import talent from "./images/talent1.png";
@@ -24,14 +25,29 @@ import pop from "./images/pop.png";
 import Child from "./Child";
 import {Link,NavLink,useNavigate}from "react-router-dom"
 function Navbar() {
+	const isDesktop = useMediaQuery({ minWidth: 992 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
     const [serch, setserch] = useState(false)
     const[find, setfind] = useState(false)
     let navigate = useNavigate();
     const[items,setItems]=useState([]);
     const[inputList,setInputList]=useState("");
 	const [showPopup, setShowPopup] = useState(true);
+	const [activeButton, setActiveButton] = useState(
+    parseInt(localStorage.getItem('activeButton')) || 1
+  );
 
-  const handleDelete = () => {
+  const handleClick = (buttonId) => {
+    setActiveButton(buttonId);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('activeButton', activeButton);
+  }, [activeButton]);
+
+	
+  	const handleDelete = () => {
     // Perform deletion logic here
     // You can update the state or trigger an API call to delete the item
 
@@ -83,22 +99,37 @@ function Navbar() {
 	navigate("/register")
   }
   return (
-    <div >
+
+
+
+
 	
-	  <div class="co">
+    <div className='mp1'>
+	  <div className="co">
 		<div className='tr'>
     <span class="top-word"style={{fontSize:"bold"}}>TM</span>
     <span class="bottom-word">true<span style={{fontSize:"25px"}}><span style={{fontSize: "25px"
-	}}className='la'>T</span>alent</span></span><p style={{marginLeft:"-590px",fontSize:'15px',marginTop:"-30px"}}>TT Essential</p><p  style={{marginLeft:"-350px",fontSize:'14px',marginTop:"-34px"}}>TT Gig</p>
+	}}className='la'>T</span>alent</span></span><p id='pot1' style={{marginLeft:"-790px",fontSize:'15px',marginTop:"-30px",marginBottom:"-20px"}}><button onClick={() => handleClick(1)}style={{ borderBottom: activeButton === 1 ? '2px solid #14BC9A' : 'none'}}id='bv1'>TT Essential</button></p><p id='pot1' style={{marginLeft:"-550px",fontSize:'14px',marginTop:"-20px",marginBottom:"10px" }}><button onClick={() => handleClick(2)}style={{borderBottom: activeButton === 2 ? '2px solid #14BC9A' : 'none'}}id='bv2'>TT Gig</button></p>
 	<table style={{marginLeft:"700px",marginBottom:"-0px",marginTop:"-31px"}}>
 		<tr><td><button onClick={improve1}className='login'>Login</button></td>
 		<td><button onClick={improve2} className='Register'>Register</button></td></tr></table>
 	  </div></div>
+	  <div id='mj'>
+	  {activeButton === 1 && (
+		<div id='hj1'>
       <h2>Get the <span className='heading'>Right Job</span> You Deserve</h2>
+	  </div>
+	   )}
+	   {activeButton === 2 && (
+		<div id='hj2'>
+		 <h2>Get the <span className='heading'>Right </span> You Deserve</h2>
+		 </div>
+		  )}
+		  </div>
       <p>Your next career move starts here</p>
 	  <div className='bt'>
-      <button  id="button" type="submit" onClick={increment}className='btn' style={{backgroundColor:serch ? "rgb(137, 214, 182)":"white"}}>Search Jobs</button>
-      <button   type="submit" onClick={decrement}className='btn' style={{backgroundColor:find ? "rgb(137, 214, 182)":"white"}}>Search Candidates</button></div>
+      <button  id="button" type="submit" onClick={increment}className='btn' style={{backgroundColor:serch ? "#14BC9A":"white"}}>Search Jobs</button>
+      <button   type="submit" onClick={decrement}className='btn' style={{backgroundColor:find ? "#14BC9A":"white"}}>Search Candidates</button></div>
       <div className='containe'>
         <div className='search-box1'>
         <FaSistrix className='search-icon'/><input type="search"id="one" className='send'placeholder='Skill/job roles'onChange={itemEvent}value={inputList}  />
@@ -110,151 +141,103 @@ function Navbar() {
 <button className='search'><span onClick={improve}>Serch</span>
 </button>
 </div><div className='head'>
-  <h1 style={{marginLeft:"450px",fontSize:"30px"}}><span style={{color:"rgb(137, 214, 182)"}}>Trusted by</span> top-tier companies</h1></div>
+  <h1 style={{marginLeft:"450px",fontSize:"30px"}}><span style={{color:"#14BC9A"}}>Trusted by</span> top-tier companies</h1></div>
   <div class="wrapper">
 	<div class="cards_wrap">
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={actu}className='img'/>
-				{/* <div class="role_name">Black Panther</div>
-				<div class="real_name">Chadwick Boseman</div>
-				<div class="film">Lorem ipsum dolor
-			</div> */}
+				
 			</div>
 		</div>
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={bim}className='img'/>
-				{/* <div class="role_name">Doctor Strange</div>
-				<div class="real_name">Benedict Cumberbatch</div>
-				<div class="film">Lorem ipsum
-				</div> */}
+				
 			</div>
 		</div>
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={cash}className='img'/>
-				{/* <div class="role_name">Black Widow</div>
-				<div class="real_name">Scarlett Johansson</div>
-				<div class="film">Lorem ipsum 
-				.</div> */}
+				
 			</div>
 		</div>
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={dro}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom</div>
-				<div class="film">Lorem 
-			</div> */}
+			
 			</div>
 		</div>
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={gro}className='img'/>
-				{/* <div class="role_name">Black Widow</div>
-				<div class="real_name">Scarlett Johansson</div>
-				<div class="film">Lorem ipsum dolor 
-		.</div> */}
+				
 			</div>
 		</div>
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={khata}className='img'/>
-				{/* <div class="role_name">Black Panther</div>
-				<div class="real_name">Chadwick Boseman</div>
-				<div class="film">Lorem ipsum 
-				.</div> */}
+				
 			</div>
 		</div>
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={nex}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-			.</div> */}
+				
 			</div>
 		</div>
 		<div class="card_item">
 			<div class="card_inner">
 				<img src={nob}className='img'/>
-				{/* <div class="role_name">Doctor Strange</div>
-				<div class="real_name">Benedict Cumberbatch</div>
-				<div class="film">Lorem 
-			</div> */}
+			
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={qd}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-				.</div> */}
+				
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={repr}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-		</div> */}
+			
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={rj}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-				.</div> */}
+				
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={w}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-				.</div> */}
+				
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={sul}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-			.</div> */}
+				
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={sws}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-		.</div> */}
+				
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={tv}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-		.</div> */}
+				
 			</div>
 		</div>
     <div class="card_item">
 			<div class="card_inner">
 				<img src={ve}className='img'/>
-				{/* <div class="role_name">Spider Man</div>
-				<div class="real_name">Tom Holland</div>
-				<div class="film">Lorem
-			.</div> */}
+				
 			</div>
 		</div>
     {/* <div class="card_item">
